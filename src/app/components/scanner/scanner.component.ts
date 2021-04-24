73b693855af6a437e6806ed774a2b49ecb3b1d26
Aravinda Rathnayake <aravindacloud@gmail.com>
2021-04-24 23:23:53 +0530

    Remove port and add media info details for ngx scanner

--- src/app/components/scanner/scanner.component.ts
+++ src/app/components/scanner/scanner.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { first } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
 import { BsModalService } from 'ngx-bootstrap/modal';
+import { BarcodeFormat } from "@zxing/library";
 
 import {Package, User} from '@/models';
 
-import {AlertService, AuthenticationService} from '@/services';
+import { AlertService, AuthenticationService } from '@/services';
 import { PackageService } from '@/services/package.service';
 
 @Component({ templateUrl: 'scanner.component.html' })
 export class ScannerComponent implements OnDestroy {
     private subscription: Subscription;
+
     public currentUser: User;
+    public deviceCurrent: MediaDeviceInfo;
+    public deviceSelected: string;
+
+    public formatsEnabled: BarcodeFormat[] = [
+        BarcodeFormat.CODE_128,
+        BarcodeFormat.DATA_MATRIX,
+        BarcodeFormat.EAN_13,
+        BarcodeFormat.QR_CODE,
+    ];
 
     constructor(private formBuilder: FormBuilder,
                 private modalService: BsModalService,
@@ -31,8 +42,11 @@ export class ScannerComponent implements OnDestroy {
             });
     }
 
-    public clearResult(): void {
-
+    public onDeviceChange(device: MediaDeviceInfo) {
+        const selectedStr = device?.deviceId || '';
+        if (this.deviceSelected === selectedStr) { return; }
+        this.deviceSelected = selectedStr;
+        this.deviceCurrent = device || undefined;
     }
 
     public ngOnDestroy(): void {
