cc451b0c32854d3326ee0cd3d6017ae3c6e0696d
Aravinda Rathnayake <aravindacloud@gmail.com>
2021-04-25 12:41:56 +0530

    Fix issue in upload packages logic

--- src/app/components/package-upload/package-upload.component.ts
+++ src/app/components/package-upload/package-upload.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Subject, Subscription } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
 import { first } from 'rxjs/operators';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { camelCase } from 'lodash/string';
@@ -15,10 +15,10 @@ export class PackageUploadComponent implements OnInit, OnDestroy {
     public packages: Package[];
 
     private subscription: Subscription;
-    private isFileLoaded: boolean = false;
 
     public saveClick = new Subject();
-    public submitted = false;
+    public loading: boolean = false;
+    public isFileLoaded: boolean = false;
 
     constructor(private formBuilder: FormBuilder,
                 public modalRef: BsModalRef,
@@ -27,17 +27,11 @@ export class PackageUploadComponent implements OnInit, OnDestroy {
     ) {
     }
 
-    public ngOnInit(): void {
-        this.packageForm = this.formBuilder.group({
-            csvFile: [null, Validators.required]
-        });
-    }
-
     private static removeDuplicatedQuotes(field) {
         return field.replace(/['"]+/g, '')
     }
 
-    public textToArray(textFields): void {
+    private textToArray(textFields): void {
         let lines = textFields.split('\n');
         let headers = lines[0].split(',');
 
@@ -58,38 +52,52 @@ export class PackageUploadComponent implements OnInit, OnDestroy {
         this.packages = result;
     }
 
-    public csvToText(file): void {
-        const reader = new FileReader();
-        reader.readAsText(file.files[0]);
-        reader.onload = () => {
-            this.textToArray(reader.result);
-            this.isFileLoaded = true;
-        }
+    private csvToText(file): Observable<boolean> {
+        return new Observable<boolean>(obs => {
+           const reader = new FileReader();
+            reader.readAsText(file.files[0]);
+            reader.onload = () => {
+                this.textToArray(reader.result);
+
+                obs.next(true);
+                obs.complete();
+            }
+        });
     }
 
-    public onSubmit(file): void {
-        this.submitted = true;
-        // stop here if form is invalid
-        if (this.packageForm.invalid) {
-            return;
-        }
-
-        this.csvToText(file);
-
-        if (!this.isFileLoaded) {
-            return;
-        }
-
+    private addPackages(): void {
         this.subscription = this.packageService.addPackageCSV(this.packages)
             .pipe(first())
             .subscribe(() => {
                     this.modalRef.hide();
                     this.alertService.success('Package list added successful', false);
+                    this.loading = false;
                     this.saveClick.next();
                 },
                 error => {
                     this.alertService.error(error);
                 });
+    }
+
+    public ngOnInit(): void {
+        this.packageForm = this.formBuilder.group({
+            csvFile: [null, Validators.required]
+        });
+    }
+
+    public onSubmit(file): void {
+        // stop here if form is invalid
+        if (this.packageForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+
+        this.csvToText(file).subscribe(status => {
+            if (status) {
+                this.addPackages()
+            }
+        });
 
         this.isFileLoaded = false;
     }
